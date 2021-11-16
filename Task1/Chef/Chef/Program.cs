be186f36f7b12d7632d3cfbefd0391600e6f8761
darkbgit <darkb@mail.com>
2021-11-16 16:22:51 +0300

    11/16/2021

--- Task1/Chef/Chef/Program.cs
+++ Task1/Chef/Chef/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Chef.Cooking;
 using Chef.Ingredients;
+using System.Collections.ObjectModel;
 
+var saladIngredients = new Collection<Vegetable>();
 
-var saladIngredients = new SaladIngredients();
-
-saladIngredients.Add(new Lettuce(100));
+saladIngredients.Add(new Lettuce(300));
 saladIngredients.Add(new Cucumber(150));
 saladIngredients.Add(new Tomato(200));
 
@@ -14,30 +14,35 @@ saladIngredients.Add(new Tomato(200));
 
 //salad.MakeSalad();
 
-saladIngredients.ToConsole();
+Console.WriteLine(saladIngredients.ToConsoleStr());
 
-bool work = true;
+bool breakFlag = true;
 
-while (work)
+while (breakFlag)
 {
     Console.WriteLine("Для сортировки по свойству введите \"-s Свойство\"");
+    Console.WriteLine("Свойства \"-k100\" - ККалорий в 100 грамм продукта");
+    Console.WriteLine("Свойства \"-k\" - ККалорий в продукте");
+    Console.WriteLine("Свойства \"-w\" - вес продукта");
     Console.WriteLine("Для поиска ингридиентов по каллорийности введите \"-с\"");
     Console.WriteLine("Для выхода введите \"exit\"");
     var input = Console.ReadLine();
+
     if (input?.Length > 1)
     {
         switch (input[..2])
         {
             case "-s":
-                var result = saladIngredients.OrderBy(i => i.Name).ToList;
-                result.ToConsole();
+                Console.WriteLine(GetSortedIngredients(input));
                 break;
             case "-c":
+                (int bottom, int top) = GetCaloricContentRange();
+                Console.WriteLine(GetIngredientsForCaloricContentRange(bottom, top));
                 break;
             case "ex":
                 if (input.Trim() == "exit")
                 {
-                    work = !work;
+                    breakFlag = !breakFlag;
                 }
                 break;
             default:
@@ -49,6 +54,90 @@ while (work)
     {
         Console.WriteLine("Неопознанная команда");
     }
+}
 
+string GetSortedIngredients(string input)
+{
+    if (input.Length > 4)
+    {
+        switch (input[3..].Trim())
+        {
+            case "-k100":
+                var k100Sort = saladIngredients
+                    .OrderBy(i => i.CaloricContentPer100Gramm)
+                    .ToList();
+                return k100Sort.ToConsoleStr();
+            case "-k":
+                var kSort = saladIngredients
+                    .OrderBy(i => i.CaloricContent)
+                    .ToList();
+                return kSort.ToConsoleStr();
 
+            case "-w":
+                var wSort = saladIngredients
+                    .OrderBy(i => i.Weight)
+                    .ToList();
+                return wSort.ToConsoleStr();
+            default:
+                return "Неправильный параметр сортировки";
+        }
+    }
+    else
+    {
+        return "Неправильный параметр сортировки";
+    }
+}
+
+(int bottom, int top) GetCaloricContentRange()
+{
+    int bottom;
+    int top;
+
+    while (true)
+    {
+        Console.WriteLine("Введите нижнию границу диапазона");
+        var input = Console.ReadLine();
+        if (int.TryParse(input, System.Globalization.NumberStyles.Number,
+            System.Globalization.CultureInfo.InvariantCulture, out bottom))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод");
+        }
+    }
+
+    while (true)
+    {
+        Console.WriteLine("Введите верхнюю границу диапазона");
+        var input = Console.ReadLine();
+
+        if (int.TryParse(input, System.Globalization.NumberStyles.Number,
+            System.Globalization.CultureInfo.InvariantCulture, out top))
+        {
+            if (bottom > top)
+            {
+                Console.WriteLine("Верхняя граница должна быть больше нижней");
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод");
+        }
+    }
+
+    return (bottom, top);
+}
+
+string GetIngredientsForCaloricContentRange(int bottom, int top)
+{
+    return saladIngredients
+        .Where(i => i.CaloricContent >= bottom && i.CaloricContent <= top)
+        .OrderBy(i => i.CaloricContent)
+        .ToList().ToConsoleStr();
 }
