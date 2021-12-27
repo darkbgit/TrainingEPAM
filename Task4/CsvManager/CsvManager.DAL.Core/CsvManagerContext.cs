9ce98479b3444985af45f01d2af2551b0907e1d2
darkbgit <darkb@mail.ru>
2021-12-27 23:03:42 +0300

    12/27/2021

---
+++ Task4/CsvManager/CsvManager.DAL.Core/CsvManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CsvManager.DAL.Core
+{
+    public class CsvManagerContext : DbContext
+    {
+        public CsvManagerContext(DbContextOptions<CsvManagerContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Task4/CsvManager/CsvManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace CsvManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json").Build();
+
+            var section = config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
